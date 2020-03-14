1dd3bcb5dc1bdd743d439e3db49d7659753d273c
nikpil93 <nikpil93@gmail.com>
2020-03-14 03:37:17 +0200

    simplified bottom nav cause the old sucked

--- src/components/BottomNav/BottomNav.js
+++ src/components/BottomNav/BottomNav.js
@@ -1,26 +1,24 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
 import BottomNavigation from "@material-ui/core/BottomNavigation"
-import Breadcrumbs from "@material-ui/core/Breadcrumbs"
 import Link from "@material-ui/core/Link"
 import Typography from "@material-ui/core/Typography"
 
 const useStyles = makeStyles({
   root: {
     position: "absolute",
-    background: "linear-gradient(63deg, rgba(219,218,240,1) 27%, rgba(175,175,209,1) 52%, rgba(80,80,204,1) 73%, rgba(27,22,128,1) 95%)",
+    backgroundColor: "#3f51b5",
     width: "100%",
     textAlign: "center"
   },
   social: {
     margin: "auto 0 auto auto",
   },
-  socialButtons: {
-    paddingRight: "10px"
-  },
   link: {
     margin: "auto 0 auto 0",
-    paddingLeft: "90px"
+    textDecoration: "none!important",
+    color: "#fff",
+    paddingLeft: "65px"
   }
 })
 
@@ -29,33 +27,12 @@ export default function LabelBottomNavigation() {
 
   return (
     <BottomNavigation className={classes.root}>
-      <Breadcrumbs aria-label="breadcrumb" className={classes.link}>
-        <Link color="inherit" href="https://technologyremastered.eu/">
-          Follow Us
+        <Link
+          className={classes.link}
+          href="https://technologyremastered.eu/">
+          Follow Us @ TechnologyRemastered
         </Link>
-        <Typography color="textPrimary">
-          &copy
-          <Link href="https://technologyremastered.eu/">
-            TechnologyRemastered
-          </Link>
-        </Typography>
-      </Breadcrumbs>
       <div className={classes.social}>
-        <img
-          className={classes.socialButtons}
-          src="https://img.icons8.com/office/16/000000/facebook.png"
-          alt="fb"
-        />
-        <img
-          className={classes.socialButtons}
-          src="https://img.icons8.com/office/16/000000/instagram.png"
-          alt="ins"
-        />
-        <img
-          className={classes.socialButtons}
-          src="https://img.icons8.com/office/16/000000/twitter.png"
-          alt="tw"
-        />
       </div>
     </BottomNavigation>
   )
